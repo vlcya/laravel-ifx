87a8dc0256cab583f987b26555cb056c16d191b2
Rabin Yasharzadehe <rabin@rabin.io>
2020-03-16 13:55:46 +0200

    phpcbf src/config/informix.php

--- src/config/informix.php
+++ src/config/informix.php
@@ -8,25 +8,25 @@
 
 return [
     'informix' => [
-        'driver'    => 'informix',
-        'host'      => env('DB_HOST', 'localhost'),
-        'database'  => env('DB_DATABASE', 'forge'),
-        'username'  => env('DB_USERNAME', 'forge'),
-        'password'  => env('DB_PASSWORD', ''),
-        'service'  => env('DB_SERVICE', '11143'),
-        'server'  => env('DB_SERVER', ''),
-        'db_locale'   => 'en_US.819',
-        'client_locale' => 'en_US.819',
-        'db_encoding'   => 'GBK',
-        'initSqls' => false,
+        'driver'          => 'informix',
+        'host'            => env('DB_HOST', 'localhost'),
+        'database'        => env('DB_DATABASE', 'forge'),
+        'username'        => env('DB_USERNAME', 'forge'),
+        'password'        => env('DB_PASSWORD', ''),
+        'service'         => env('DB_SERVICE', '11143'),
+        'server'          => env('DB_SERVER', ''),
+        'db_locale'       => 'en_US.819',
+        'client_locale'   => 'en_US.819',
+        'db_encoding'     => 'GBK',
+        'initSqls'        => false,
         'client_encoding' => 'UTF-8',
-        'prefix'    => ''
+        'prefix'          => '',
     ],
 
     'informix-source-json' => [
-        'driver'    => 'informix-json',
-        'uri'      => 'http://XX.X.XX.XX:9999/json',
-        'source'  => 'source',
+        'driver' => 'informix-json',
+        'uri'    => 'http://XX.X.XX.XX:9999/json',
+        'source' => 'source',
         'token'  => 'SDL3490FI2902309DSFK203SDKL2334202',
     ],
 ];
